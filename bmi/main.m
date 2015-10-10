6a2676db58ec048d00d93ff17da1c2b8d2f44c76
hogle <hogle@hogles-Mac.local>
2015-10-10 09:37:50 +0900

    Initial Commit

---
+++ bmi/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  bmi
+//
+//  Created by hogle on 2015. 10. 10..
+//  Copyright (c) 2015년 hogle. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
